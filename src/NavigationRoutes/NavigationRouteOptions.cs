74cee97b46fbc19ceab2d85ef200be3387cfefff
James Chambers <james@jameschambers.com>
2013-02-23 13:06:23 -0800

    Missed files

---
+++ src/NavigationRoutes/NavigationRouteOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavigationRoutes
+{
+    public class NavigationRouteOptions
+    {
+        public string Area { get; set; }
+        public string FilterToken { get; set; }
+    }
+}
